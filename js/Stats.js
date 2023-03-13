3e142650754519bf02a2922f8d6decc8defe1b61
FabiRomano <99614961+FabiRomano@users.noreply.github.com>
2023-03-13 09:26:51 -0300

    aff

--- js/Stats.js
+++ js/Stats.js
@@ -11,7 +11,18 @@ console.log(currentDate);
 console.log(totalData);
 
 
+//filtro los eventos con asistencia
+let filtAsistencia = events.filter((filtAsis) => filtAsis.assistance)
+console.log(filtAsistencia);
 
+//mapeo la asistencia 
+let mapeoAsistencia = filtAsistencia.map((asist) => asist.assistance)
+console.log(mapeoAsistencia);
+
+
+//mapeo la capacidad de todos los eventos
+let mapCapacidad = events.map((cap) => cap.capacity)
+console.log(mapCapacidad);
 
 
 
